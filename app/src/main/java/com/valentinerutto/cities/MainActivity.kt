37b909536a98a586059e1e1dd75583858e7ff2b0
ValentinRutto <valentine.rutto@dlight.com>
2022-04-11 10:01:17 +0300

    -Get list of cities

--- app/src/main/java/com/valentinerutto/cities/MainActivity.kt
+++ app/src/main/java/com/valentinerutto/cities/MainActivity.kt
@@ -2,6 +2,8 @@ package com.valentinerutto.cities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
 import com.valentinerutto.cities.databinding.ActivityMainBinding
 import com.valentinerutto.citiesoftheworld.CitiesViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -16,6 +18,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupObservables()
+        citiesViewModel.init(page = currentPage, isFreshStart = true)
+
     }
+
+    private fun setupObservables() {
+        citiesViewModel.isLoading.observe(this) { showLoading ->
+            binding.citiesProgressBar.isVisible = showLoading
+        }
+
+        citiesViewModel.errorCitiesResponse.observe(this) { errorMsg ->
+            binding.citiesErrorTextView.text = errorMsg
+        }
+
+        citiesViewModel.successfulCitiesResponse.observe(this) { citiesResponse ->
+            binding.citiesRecyclerView.adapter = CitiesAdapter().apply {
+                submitList(citiesResponse)
+            }
+            binding.citiesRecyclerView.addOnScrollListener(
+                object : RecyclerView.OnScrollListener() {
+
+                    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                        super.onScrolled(recyclerView, dx, dy)
+                        if (dy > 0) {
+                            val nextPage = currentPage + 1
+                            if (nextPage < citiesViewModel.getTotalPages()) {
+                                citiesViewModel.init(page = nextPage, isFreshStart = false)
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    }
+
 
 }
